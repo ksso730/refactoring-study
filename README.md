247ac545d6e2af019ca873b3e0678e31a40a6727
ksso730 <ksso7300@gmail.com>
2022-01-23 19:42:28 +0900

    계산 단계와 포매팅 단계 파일분리

--- README.md
+++ README.md
@@ -1,21 +1,27 @@
 # REFACTORING
 > 리펙터링 2판 -마틴파울러 를 스터디.
 
-### 아하⁉️
-> - 반복문 쪼개기
+## 1. 리펙터링 첫번째 예시
+> __1. 반복문 쪼개기__
+```
     반복문이 쪼개져 있는 경우 오히려, 중복된 반복문을 없애기 바빴는데. 쪼개는 것이 리팩터링이었다.
     최적화 이전에 리펙터링이 먼저. 이후에 문제가 생길 경우 다시 하나의 반복문으로 수정하면 된다.
-
-> 1. 반복문 쪼개기
-2. 문장 슬라이드 하기: 변수 초기화 문장을 변수값 누적 코드 바로 앞으로 옮긴다.
-3. 함수 추출하기
-4. 변수 인라인하기: 임시변수를 제거하고 질의함수로 대체
+``` 
+2. __문장 슬라이드 하기__
+```
+    변수 초기화 문장을 변수값 누적 코드 바로 앞으로 옮긴다.
+```
+3. __함수 추출하기__
+4. __변수 인라인하기__
+```
+    임시변수를 제거하고 질의함수로 대체
+```
 
 ---
 
-### 참고
+## 참고
 
-- json 파일에서 값 읽어오기.
+### json 파일에서 값 읽어오기.
 ```js
 const require = createRequire(import.meta.url);
 const invoicesData = require("./data/invoices.json");
@@ -23,4 +29,12 @@ const playData = require("./data/play.json");
 
 const invoices = JSON.parse(JSON.stringify(invoicesData));
 const plays = JSON.parse(JSON.stringify(playData));
+```
+
+### reduce 함수 : 배열의 각 요소를 순회. callback 함수의 실행값을 누적하여 반환.
+	 .__reduce__((accumulator, currentValue, index, array )=> accumulator + currentValue, initialValue);
+     (optional: index, array)
+```js
+data.performances
+            .reduce((total, perf)=> total+perf.amount, 0);
 ```
--- ch1/src/main.js
+++ ch1/src/main.js
@@ -1,5 +1,5 @@
 import {createRequire} from "module";
-import { format } from "path";
+import { createStatementData } from "./createStatement.js";
 
 const require = createRequire(import.meta.url);
 const invoicesData = require("./data/invoices.json");
@@ -8,102 +8,32 @@ const playData = require("./data/play.json");
 const invoices = JSON.parse(JSON.stringify(invoicesData));
 const plays = JSON.parse(JSON.stringify(playData));
 
-const renderPlainText = (data) => {
-    
-    const usd = (aNumber) => {
-        return new Intl.NumberFormat("en-US", 
-                            { style: "currency", currency: "USD",
-                            minimumFractionDigits: 2}).format(aNumber/100);
-    }
+function usd (aNumber) {
+    return new Intl.NumberFormat("en-US", 
+                        { style: "currency", currency: "USD",
+                        minimumFractionDigits: 2}).format(aNumber/100);
+}
 
-    let result = `=== 청구내역 (${data.customer})\n`;
-
+const renderHtml = (data) => {
+    let result = `<h1>청구내역 (${data.customer})</h1>\n`;
+    result += "<table>\n";
+    result += "<tr><th>연극</th><th>좌석 수</th><th>금액</th></tr>";
     for (let perf of data.performances) {
-        // 청구내역 출력
-        result += `\t${perf.play.name}: ${usd(perf.amount)} (${perf.audience}석)\n`;
-    }    
+        result += `<tr><td>${perf.play.name}</td><td>${perf.audience}석</td>`
+        result += `<td>${usd(perf.amount)}</td></tr>\n`;
+    } 
+    result += "</table>\n";
     
-    result += `총액: ${usd(data.totalAmount)}\n`;
-    result += `적립포인트: ${data.totalVolumeCredits}점\n`;
+    result += `<p>총액: <em>${usd(data.totalAmount)}</em></p>\n`;
+    result += `<p>적립포인트: <em>${data.totalVolumeCredits}점</em></p>\n`;
     return result;
 }
 
-// 데이터 모으기
-const enrichPerformance = (aPerformance) => {
-    const playFor = (aPerformance) => {
-        return plays[aPerformance.playID];
-    }
-
-    const amountFor = (aPerformance) => {
-        let result = 0;
-
-            switch (aPerformance.play.type) {
-                case "tragedy":
-                    result = 40000;
-                    if (aPerformance.audience > 30) {
-                            result += 1000 *(aPerformance.audience -30);
-                        }
-                    break;
-                case "comedy":
-                    result = 30000;
-                    if (aPerformance.audience > 20) {
-                        result += 10000 + 500 *(aPerformance.audience -20);
-                    }
-                    result += 300 * aPerformance.audience;
-                    break;
-                default:
-                    throw new Error(`알 수 없는 장르: ${aPerformance.play.type}`);
-            }
-        return result;
-    }
-
-    const volumeCreditsFor = (aPerformance) => {
-        let result = 0;
-        //포인트 적립
-        result += Math.max(aPerformance.audience -30, 0);
-        // 희극 관객 5명마다 추가 포인트 제공
-        if ("comedy" == aPerformance.play.type)
-            result += Math.floor(aPerformance.audience/5);
-        return result;
-    }
-
-
-    const result = Object.assign({}, aPerformance);
-    result.play = playFor(result);
-    result.amount = amountFor(result);
-    result.volumeCredits = volumeCreditsFor(result);
-    return result;
-}
-
-const statement = (invoice) => {
-    
-    const totalAmount = (data) => {
-        let result = 0;
-        for (let perf of data.performances) {
-            // 청구내역 출력
-            result += perf.amount;
-        }
-        return result;
-    }
-
-    const totalVolumeCredits = (data) => {
-        let volumeCredits = 0;
-        for (let perf of data.performances) {
-            //포인트 적립
-            volumeCredits += perf.volumeCredits;
-        }
-        return volumeCredits;
-    }
-
-    const statementData = {};
-    statementData.customer = invoice.customer;
-    statementData.performances = invoice.performances.map(enrichPerformance);
-    statementData.totalAmount = totalAmount(statementData);
-    statementData.totalVolumeCredits = totalVolumeCredits(statementData);
-    return renderPlainText(statementData, plays);
+const htmlStatement = (invoice, plays) => {
+    return renderHtml(createStatementData(invoice, plays));
 }
 
 for (let invoice of invoices){
-    const result = statement(invoice);
+    const result = htmlStatement(invoice, plays);
     console.log(result);
 }
